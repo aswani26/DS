83d31db30020dd4bd264391f86046491c2fa1b8d
aswani26 <aswania9365@gmail.com>
2025-11-25 22:39:49 +0530

    kruskals.c

--- kruskals.c
+++ kruskals.c
@@ -1,44 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define SIZE 10
-
-void main (){
-int u,v,n,i,j,ne=1;
-int visited[10]={0},min,mincost=0,cost[10][10];
-
-printf("\n enter the number of nodes:");
-scanf("%d",&n);
-
-printf("\n enter the adjacent matrix: \n");
-for (i=1;i<=n;i++)
-for (j=1;j<=n;j++)
-{ scanf("%d",&cost[i][j]);
-if(cost[i][j]==0)
-cost[i][j]=999;
+#include <stdio.h>
+int i, j, u, v,rv,ru,n, ne = 1;
+int min, mincost = 0;
+int cost[10][10], parent[10];
+int find(int i) {
+    while (parent[i])
+        i = parent[i];
+    return i;
+}
+void union_sets(int i, int j) {
+    if (i != j)
+        parent[j] = i;
 }
 
-visited[1]=1;
-printf("\n");
+int main() {
 
-while(ne<n)
-{
-min=999;
+  printf("Enter number of vertices: ");
+  scanf("%d", &n);
 
-for(i=1;i<=n;i++)
-for(j=1;j<=n;j++)
-if (cost[i][j] < min)
-if (visited[i] != 0 && visited[j] == 0)
-{
-min = cost[i][j];
-u = i;
-v = j;
+  printf("Enter the cost adjacency matrix:\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            scanf("%d", &cost[i][j]);
+            if (cost[i][j] == 0)
+                cost[i][j] = 999;  
+        }
+    }
+
+    printf("The edges of the Minimum Cost Spanning Tree are:\n");
+
+    while (ne < n) {
+        min = 999;
+
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                if (cost[i][j] < min) {
+                    min = cost[i][j];
+                    u = i;
+                    v = j;
+          }
+         }
+        }
+
+        ru = find(u);
+        rv = find(v);
+
+        if (ru != rv) {
+            union_sets(ru, rv);
+            printf("%d edge (%d, %d) = %d\n", ne++, u, v, min);
+            mincost += min;
+        }
+
+        cost[u][v] = cost[v][u] = 999;
+    }
+
+    printf("\nMinimum cost = %d\n", mincost);
+
+    return 0;
 }
-printf("\n edge %d : (%d %d) cost : %d",ne++,u,v,min);
-mincost += min;
-visited[v] = 1;
-cost[u][v] = cost[v][u] = 999;
-}
-
-printf("\n minimum cost = %d",mincost);
-}
-
